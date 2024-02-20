3d13ca878f506ecc7d951db82924f038dc845501
francielebueno <68562287+francielebueno@users.noreply.github.com>
2024-02-19 22:19:24 -0300

    Exercício 2 - Semana 4

---
+++ Exe2/script.js
@@ -0,0 +1,6 @@
+// Declare uma variável e atribua um número a ela, em seguida, exiba seu valor no console.
+
+// Atribua o valor a variável usando o window.prompt();
+
+var resultado = window.prompt("Digite um número", "");
+console.log(resultado);
