fb6b79201af516f302f6e70dd81fb87a31bc1fb0
francielebueno <68562287+francielebueno@users.noreply.github.com>
2024-02-19 22:17:26 -0300

    Exercício 1 - Semana 4

---
+++ Exe1/script.js
@@ -0,0 +1,9 @@
+// M1S04 - Exercicio 01 
+// Crie uma função que exiba uma mensagem simples no console.
+
+
+function exibirMensagem() {
+    console.log("Primeiro exercício");
+}
+
+exibirMensagem();
