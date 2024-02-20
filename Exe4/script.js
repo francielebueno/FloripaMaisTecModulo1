68a9baef542bf9bdc8bc66534f6b6b9024288529
francielebueno <68562287+francielebueno@users.noreply.github.com>
2024-02-19 22:20:45 -0300

    Exercício 4 - Semana 4

---
+++ Exe4/script.js
@@ -0,0 +1,8 @@
+//Crie uma função que receba dois números como parâmetros e retorne sua soma no console.log.
+function somaDoisNumeros(primeiroNumero, segundoNumero){
+    return primeiroNumero + segundoNumero;
+}
+
+var resultado = somaDoisNumeros(4, 5);
+
+console.log(resultado);
