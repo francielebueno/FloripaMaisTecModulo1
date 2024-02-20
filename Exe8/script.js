8fe45fe46ab5c7c9122d3107166fad22cf09e411
francielebueno <68562287+francielebueno@users.noreply.github.com>
2024-02-19 22:22:21 -0300

    Exercício 8 - Semana 4

---
+++ Exe8/script.js
@@ -0,0 +1,14 @@
+function parImpar(numero){
+
+    let resto = numero%2;
+    
+    if (resto === 0){
+    console.log(numero + ": Este número é par!"); }
+    else {
+    console.log(numero + ": Este número é ímpar!");
+    }
+
+}
+
+parImpar(16);
+parImpar(7);
