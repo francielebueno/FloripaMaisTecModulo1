40c5caec512af8d83c5a3763b69caf677565163e
francielebueno <68562287+francielebueno@users.noreply.github.com>
2024-02-19 22:20:04 -0300

    Exercício 3 - Semana 4

---
+++ Exe3/script.js
@@ -0,0 +1,8 @@
+// Combine duas strings usando o operador de concatenação e exiba o resultado no console.
+
+// Utilize o template string ou a concatenação simples.
+
+var primeiraString = "Franciele";
+var segundaString = "Bueno";
+
+console.log(primeiraString + " " + segundaString);
