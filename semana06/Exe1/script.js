46cc86dbb4de94d10510128c53ccec65d2e07f55
franciele.bueno <franciele.bueno@gmail.com>
2024-03-05 16:25:44 -0300

    Exercício 8

--- semana06/Exe1/script.js
+++ semana06/Exe1/script.js
@@ -1,3 +1,18 @@
 function exibirMensagem () {
     alert("Bem-vindo(a) ao meu perfil!");
 }
+
+var coresDiversas = ['purple', 'blue', 'black'];
+
+var elemento = document.querySelector('footer');
+
+var indiceCorAtual = 0;
+
+function alternarCoresPrimarias() {
+
+    elemento.style.color = coresDiversas[indiceCorAtual];
+
+    indiceCorAtual = (indiceCorAtual + 1) % coresDiversas.length;
+}
+
+setInterval(alternarCoresPrimarias, 1000);
